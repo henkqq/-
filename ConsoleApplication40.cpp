b1f84c3ac60ebf05dbb526f3204723c7baffd655
are you ladiesman 217? <superdmitryj@gmail.com>
2025-12-19 11:42:29 +0300

    Практическая W3

---
+++ ConsoleApplication40.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(0, "");
+	int num;
+	int sum = 0;
+	int u = 0;
+	cout << "напиши число: ";
+	cin >> num;
+	while (num != 0) {
+		sum += num;
+		cout << "введите ещё число: ";
+		cin >> num;
+		u++;
+	}
+	cout << "сумма: " << sum << endl;
+	cout << "Количество чисел: " << u << endl;
+	return 0;
+}
